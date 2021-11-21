92a4dea94ee82095da8c1eebe7143e930f9885d0
chaudharyachint08 <chaudharyachint08@gmail.com>
2021-11-21 16:31:32 +0530

    Standard processes as per RMTPP

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,7 @@
-from scipy.integrate import integrate
+import numpy as np
+from scipy import integrate
+
+import matplotlib.pyplot as plt
 
 # class numeric_integration:
 #     def __init__(self, name):
@@ -14,7 +17,7 @@ from scipy.integrate import integrate
 #     'Should be performed using Tensorflow Numerical integration'
 
 class SyntheticGenerativeProcess:
-    def __init__(self, name, epsilon=1e-7. **model_params):
+    def __init__(self, name, epsilon=1e-7, **model_params):
         '''
         poisson
             Constant Intensity Function, intensity set to 'mu'
@@ -30,48 +33,52 @@ class SyntheticGenerativeProcess:
         if self.name=='poisson':
             self.param_mu = model_params['mu']
         if self.name=='hawkes':
-            self.param_mu, self.param_w = model_params['mu'], model_params['w']
+            self.param_mu, self.param_alpha, self.param_beta = model_params['mu'], model_params['alpha'], model_params['beta']
         if self.name=='self-correcting':
-            pass
-        if self.name=='poisson':
-            pass
+            self.param_mu, self.param_alpha = model_params['mu'], model_params['alpha']
+        if self.name=='auto-regressive-conditional-duration':
+            self.param_mu, self.param_alpha = model_params['mu'], model_params['alpha']
     
     def intensity(self, timestamp, seq_timesteps=[]):
         'Modeling the ultimate intensity function used in literature for statistical TPPs'
+        if timestamp==np.inf or np.inf in seq_timesteps:
+            raise Exception('Infinity as timestamp reached')
         if self.name=='poisson':
             return self.param_mu
         if self.name=='hawkes':
-            pass
+            return self.param_mu + self.param_alpha * np.exp(-self.param_beta*(timestamp-np.array(seq_timesteps))).sum()
         if self.name=='self-correcting':
-            pass
-        if self.name=='poisson':
-            pass
+            return np.exp(self.param_mu*timestamp - self.param_alpha*len(seq_timesteps))
+        if self.name=='auto-regressive-conditional-duration':
+            seq_timesteps = np.array(seq_timesteps)
+            return 1/(self.param_mu + self.param_alpha*(seq_timesteps[1:]-seq_timesteps[:-1]).sum())
         return intensity_value
     
-    def probability(self, timestamp, seq_timesteps=[]):
+    def survived_intensity(self, timestamp, seq_timesteps=[], survival_bound_function=lambda x:np.exp(-x)):
         'This function does cross product of intensity_function with survival_probability* at given time'
         intensity_value = self.intensity(timestamp, seq_timesteps)
+        if timestamp==np.inf or np.inf in seq_timesteps:
+            raise Exception('Infinity as timestamp reached')
         if seq_timesteps :
-            time_gap = timestamp - seq_timesteps[-1]
+            time_gap = timestamp - seq_timesteps[-1]            
             if not time_gap :
                 survival_value = 0
             elif time_gap < 0 :
                 raise Exception(f'Current timestep {timestamp} cannot be lesser than last timestep {seq_timesteps[-1]}')
             elif time_gap > 0 :
-                survival_value = integrate.quad(self.intensity, seq_timesteps[-1], timestamp)
+                survival_value, _ = integrate.quad(lambda x:self.intensity(x,seq_timesteps), seq_timesteps[-1], timestamp)
         else:
             survival_value = 1
-        probability_value = intensity_value * survival_value
-        return probability_value
-        
-        
+        survived_intensity_value = intensity_value * survival_bound_function(survival_value)
+        return survived_intensity_value
 
     def generate(self, seq_len):
         if seq_len <= 0:
             raise Exception(f'Sequence length to be generate should be greater than 0')
         seq_timesteps = []
         while len(seq_timesteps)<seq_len:
-            next_timestamp = integrate.quad(self.intensity, seq_timesteps[-1], timestamp)
+            prev_timestamp    = seq_timesteps[-1] if seq_timesteps else 0
+            next_timestamp, _ = integrate.quad(lambda x:x*self.survived_intensity(x, seq_timesteps), prev_timestamp, np.inf)
             seq_timesteps.append( next_timestamp )
         min_time, max_time = min(seq_timesteps), max(seq_timesteps)
-        return seq_timesteps
+        return np.array(seq_timesteps)
